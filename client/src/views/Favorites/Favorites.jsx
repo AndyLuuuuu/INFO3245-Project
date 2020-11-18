24550ecaec2dff50cfffc9221483c6e73bb75c6d
Wilson Wong <wilsonwong2988@gmail.com>
2020-11-17 19:27:05 -0800

    Add sign out function

--- client/src/views/Favorites/Favorites.jsx
+++ client/src/views/Favorites/Favorites.jsx
@@ -1,15 +1,22 @@
 import React, { useContext } from 'react'
 import { Container } from './FavoritesStyled'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import { UserContext } from '../../data/UserContext';
 
-export default function Favorites() {
+export default function Favorites(props) {
     const user = useContext(UserContext);
+
+    const signOut = () => {
+        user.setUserId = '';
+        user.setUsername = '';
+        props.navigation.navigate('Login');
+    }
 
     return (
         <Container>
             <Text>{user.userId}</Text>
             <Text>Welcome back {user.username}</Text>
+            <Button title="Sign Out" onPress={signOut} />
         </Container>
     )
 }
